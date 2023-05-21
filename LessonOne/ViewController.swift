7ef841922257b0e58d3908c77a2402822ecf12ea
Nelepizza <nelepizza@gmail.com>
2023-05-21 22:57:28 +0200

    Setup UI and Work with logic

--- LessonOne/ViewController.swift
+++ LessonOne/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var greetingLabel: UILabel!
+    
+    @IBOutlet var greetingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greetingBottonTap() {
+        greetingLabel.isHidden.toggle()
+        
+ //       if greetingLabel.isHidden {
+ //           greetingButton.setTitle("Показать приветствие", for: .normal)
+ //       } else {
+ //           greetingButton.setTitle("Скрыть приветствие", for: .normal)
+ //       }
+      
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Показать приветствие" : "Скрыть приветствие",
+            for: .normal
+        )
+        
+    }
+    
 }
 
